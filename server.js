b2284a12fcf335d0e4f430331764e84b5b5dda6d
Duder <thedudeabides1981@hotmail.com>
2022-09-19 16:54:39 -0700

    reordered consts

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const { animals } = require('./data/animals.json');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
-const { animals } = require('./data/animals.json');
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -103,6 +104,6 @@ app.post('/api/animals', (req, res) => {
     }
 });
 
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
